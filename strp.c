15ca9bf5f0d7d3bbe4988b4d17691298577655f5
Rain Huang <rh2378@princeton.edu>
2024-10-03 19:07:11 -0400

    comments

--- strp.c
+++ strp.c
@@ -9,7 +9,7 @@ size_t Str_getLength(const char* s) {
     const char *cur;
     assert(s != NULL);
     cur = s;
-    while(*cur != '\0') {
+    while(*cur != '\0') { /*Increments pointer until we reach the end*/
         cur++;
     } 
     return (size_t)(cur - s);
@@ -23,11 +23,11 @@ char* Str_copy(char* copy, const char* original) {
     originalCur = original;
     copyCur = copy;
     while(*originalCur != '\0') {
-        *copyCur = *originalCur;
+        *copyCur = *originalCur; 
         originalCur++;
         copyCur++;
     }
-    *copyCur = '\0';
+    *copyCur = '\0'; /*Re-adds null terminator*/
     return (char*)copy;
 }
 
@@ -37,13 +37,13 @@ char* Str_concat(char* copy, const char* original) {
     assert(original != NULL);
     assert(copy != NULL);
     originalCur = original;
-    copyCur = copy + (int)Str_getLength(copy);
+    copyCur = copy + (int)Str_getLength(copy); /*iteration starting pt*/
     while(*originalCur != '\0'){
         *copyCur = *originalCur;
         originalCur++;
         copyCur++;
     }
-    *copyCur = '\0';
+    *copyCur = '\0'; /*Re-adds null terminator*/
     return (char*)copy;
 }
 
@@ -56,20 +56,21 @@ int Str_compare(const char* s1, const char* s2) {
     s2Cur = (char*)s2;
     while(*s1Cur != '\0' && *s2Cur != '\0') {
         if((int)(*s1Cur - *s2Cur) != 0) {
-            return (int)(*s1Cur - *s2Cur);
+            return (int)(*s1Cur - *s2Cur); /*returns correct sign int*/
         }
         s1Cur++;
         s2Cur++;
     }
-    return (int)(*s1Cur - *s2Cur);
+    return (int)(*s1Cur - *s2Cur); /*comparing last character of
+    shorter string out of s1 and s2*/
 }
 
 char* Str_search(const char* toSearch, const char* find) {
     int searchLength;
     int findLength;
-    char* toSearchCur;
-    char* toSearchComparator;
-    char* findComparator;
+    char* toSearchCur; /*current pointer for start of search*/
+    char* toSearchComparator; /*to be compared to find*/
+    char* findComparator; /*target character*/
     assert(toSearch != NULL);
     assert(find != NULL);
     searchLength = (int)Str_getLength(toSearch);
@@ -79,20 +80,20 @@ char* Str_search(const char* toSearch, const char* find) {
         return (char*)toSearch;
     }
     while(toSearchCur <= toSearch + searchLength - findLength) {
-        int hasFound = 1;
+        int hasFound = 1; /*flag set to true*/
         toSearchComparator = toSearchCur;
-        findComparator = (char*)find;
+        findComparator = (char*)find; /*initialize matching pointers*/
         while(findComparator < find + findLength) {
             if(*toSearchComparator != *findComparator) {
-                hasFound = 0;
+                hasFound = 0; /*discrepancy, flag is false*/
             }
             findComparator++;
-            toSearchComparator++;
+            toSearchComparator++; /*increment to match next character*/
         }
         if(hasFound) {
-            return (char*)toSearchCur;
+            return (char*)toSearchCur; /*no discrepancy!*/
         }
         toSearchCur++;
     }
-    return NULL;
+    return NULL; /*discrepancy means null*/
 }
