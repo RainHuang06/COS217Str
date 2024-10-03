b2e0f61ea9359aa97e0640e9e3e2d593aa8c907c
Rain Huang <rh2378@princeton.edu>
2024-10-03 16:09:01 -0400

    Improved efficiency and added comments

--- stra.c
+++ stra.c
@@ -1,49 +1,79 @@
+/*
+stra.c
+Author: Rain Huang
+*/
 #include <stddef.h>
 #include <assert.h>
 #include "str.h"
+/*
+Takes one argument, const char s[]. Returns the length of the string up to the first null terminator. Errors
+if argument is null.
+*/
 size_t Str_getLength(const char s[]) {
     size_t length = 0;
     assert(s != NULL);
     while(s[length] != '\0') {
         length++;
-    }
+    } 
     return length;
 }
+/*
+Takes two arguments, char copy[] and const char original[]. Copies values of original to copy and then returns
+the characters copied.
+*/
 char* Str_copy(char copy[], const char original[]) {
-    int length = (int)Str_getLength(original);
     int i;
     assert(original != NULL);
-    for(i = 0; i <= length; i++) {
+    assert(copy != NULL);
+    while(original[i] != '\0') {
         copy[i] = original[i];
+        i++;
     }
     return (char*)copy;
 }
+/*
+Takes two arguments, char copy[] and const char original[]. Concatenates values of original to the end of copy[],
+then returns the resulting string.
+*/
 char* Str_concat(char copy[], const char original[]) {
     int copyStart = (int)Str_getLength(copy);
-    int length = (int)Str_getLength(original);
-    int i;
+    int i = copyStart;
     assert(original != NULL);
-    for(i = copyStart; i <= copyStart + length; i++) {
+    assert(copy != NULL);
+    while(original[i - copyStart] != '\0') { /*Copies until end of string*/
         copy[i] = original[i - copyStart];
+        i++;
     }
+    copy[i] = '\0';
     return (char*)copy;
 }
+/*
+Takes two arguments, const char s1[] and const char s2[]. Compares the lexicographical value
+ie alphabetical order, then returns -1 if s1 is smaller than s2.
+*/
 int Str_compare(const char s1[], const char s2[]) {
-    int s1Length = (int)Str_getLength(s1);
-    int s2Length = (int)Str_getLength(s2);
-    int lesserLength = s1Length < s2Length ? s1Length : s2Length;
     int i;
     assert(s1 != NULL);
     assert(s2 != NULL);
-    for(i = 0; i <= lesserLength; i++) {
-        if(s1[i] < s2[i]) {
-            return -1;
-        } else if(s1[i] > s2[i]) {
-            return 1;
+    for(i = 0; s1[i] != '\0' && s2[i] != '\0'; i++) {
+        if(s1[i] - s2[i] != 0) {
+            return s1[i] - s2[i]; /*Comparison of unequal character value of correct sign*/
         }
     }
-    return 0;
+    if(s1[i] == '\0' && s2[i] == '\0') { /*Handles end-of-string cases*/
+        return 0; /*Are the strings of equal length?*/
+    } else if(s1[i] == '\0') {
+        return -1; /*Shorter string comes first*/
+    } else {
+        return 1;
+    }
+    
 }
+/*
+Takes two arguments, const char toSearch[] and const char find[].
+If searching for an empty string, returns toSearch.
+If nothing was found, returns a null pointer.
+*/
 char* Str_search(const char toSearch[], const char find[]) {
     int searchLength = (int)Str_getLength(toSearch);
     int findLength = (int)Str_getLength(find);
@@ -66,37 +96,3 @@ char* Str_search(const char toSearch[], const char find[]) {
     }
     return NULL;
 }
-/*int main(int argc, char* argv[]) {
-    printf("%zd", Str_getLength(""));
-    char nullTerminatorTest[] = {'\0', 'a', '\0', '\0'};
-    printf("%zd", strlen(nullTerminatorTest));
-    printf("%zd", Str_getLength(nullTerminatorTest));
-    printf("\n");
-    char destination[10];
-    char original[] = "abcdefghi";
-    char compEqual[] = "abcdefghi";
-    char compLessThanOriginalNormal[] = "aaaaaaaaa";
-    char compLessThanOriginalShort[] = "abc";
-    char compMoreThanOriginal[] = "zzzzzzzzz";
-    char concatenator[] = "jklm";
-    char dest2[13] = "abcdefghi";
-    char toFind[] = "def";
-    Str_copy(destination, original);
-    printf(destination);
-    Str_concat(concatenator, dest2);
-    printf("\n");
-    printf(concatenator);
-    printf("\n");
-    printf("%d", Str_compare(original, compEqual));
-    printf("%d", Str_compare(original, compLessThanOriginalNormal));
-    printf("%d", Str_compare(original, compLessThanOriginalShort));
-    printf("%d", Str_compare(original, compMoreThanOriginal));
-    printf("\n");
-    printf("%c", *Str_search(original, toFind));
-    printf("%p", Str_search(original, "qazzawazza"));
-    printf("%c", *Str_search(original, "ghi"));
-    printf("%c", *Str_search(original, "abc"));
-    printf("%c", *Str_search(original, ""));
-    printf("%c", *strstr(original, ""));
-}
-*/
