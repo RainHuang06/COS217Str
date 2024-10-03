af1dd4f3769349ccaeead87d2f062f91718cdd96
Rain Huang <rh2378@princeton.edu>
2024-10-03 16:51:00 -0400

    more descriptive comments

--- str.h
+++ str.h
@@ -5,17 +5,21 @@ Author: Rain Huang
 #ifndef STRING_H
 #define STRING_H
 #include <stddef.h>
-/*Returns the length of string s*/
+/*Returns the length of string s, EXCLUDING null terminator*/
 size_t Str_getLength(const char s[]);
-/*Copies original to copy, then returns the copied string*/
+/*Copies original to copy, then returns copy*/
 char* Str_copy(char copy[], const char original[]);
-/*Concatenates original to the end of copy, then returns the resulting string.*/
+/*Concatenates original to the end of copy, then returns copy.
+Overwrites the null terminator.*/
 char* Str_concat(char copy[], const char original[]);
-/*Compares s1 and s2 by lexicographical order, returning a negative value if s1 is lexicographically less than s2,
-a positive value if s1 is lexicographically more than s2, and 0 if they are equivalent.
-*/
+/*Compares s1 and s2 by lexicographical order, returning a negative
+integer value if s1 is lexicographically less than s2,
+a positive value if s1 is lexicographically more than s2,
+and 0 if they are equivalent.*/
 int Str_compare(const char s1[], const char s2[]);
-/*Searches for string find in string toSearch, and returns pointer to first occurrence. Returns a null pointer if
-there is no ocurrences of find in toSearch, and returns toSearch if find is null.*/
+/*Searches for string find in string toSearch, and returns pointer to
+first occurrence. Returns a null pointer if
+there is no ocurrences of find in toSearch, and returns 
+toSearch if find is empty string.*/
 char* Str_search(const char toSearch[], const char find[]);
 #endif
