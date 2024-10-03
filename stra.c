bb9c6dd0dd937a04cf3e04a3e496005cc671924d
Rain Huang <rh2378@princeton.edu>
2024-10-03 16:50:45 -0400

    fixed type mismatch

--- stra.c
+++ stra.c
@@ -5,10 +5,6 @@ Author: Rain Huang
 #include <stddef.h>
 #include <assert.h>
 #include "str.h"
-/*
-Takes one argument, const char s[]. Returns the length of the string up to the first null terminator. Errors
-if argument is null.
-*/
 size_t Str_getLength(const char s[]) {
     size_t length = 0;
     assert(s != NULL);
@@ -18,10 +14,6 @@ size_t Str_getLength(const char s[]) {
     return length;
 }
 
-/*
-Takes two arguments, char copy[] and const char original[]. Copies values of original to copy and then returns
-the characters copied.
-*/
 char* Str_copy(char copy[], const char original[]) {
     int i = 0;
     assert(original != NULL);
@@ -34,10 +26,6 @@ char* Str_copy(char copy[], const char original[]) {
     return (char*)copy;
 }
 
-/*
-Takes two arguments, char copy[] and const char original[]. Concatenates values of original to the end of copy[],
-then returns the resulting string.
-*/
 char* Str_concat(char copy[], const char original[]) {
     int copyStart = (int)Str_getLength(copy);
     int i = copyStart;
@@ -51,17 +39,13 @@ char* Str_concat(char copy[], const char original[]) {
     return (char*)copy;
 }
 
-/*
-Takes two arguments, const char s1[] and const char s2[]. Compares the lexicographical value
-ie alphabetical order, then returns -1 if s1 is smaller than s2.
-*/
 int Str_compare(const char s1[], const char s2[]) {
     int i;
     assert(s1 != NULL);
     assert(s2 != NULL);
     for(i = 0; s1[i] != '\0' && s2[i] != '\0'; i++) {
-        if(s1[i] - s2[i] != 0) {
-            return s1[i] - s2[i]; /*Comparison of unequal character value of correct sign*/
+        if((int)(s1[i] - s2[i]) != 0) {
+            return (int)(s1[i] - s2[i]); /*Comparison of unequal character value of correct sign*/
         }
     }
     if(s1[i] == '\0' && s2[i] == '\0') { /*Handles end-of-string cases*/
@@ -74,11 +58,6 @@ int Str_compare(const char s1[], const char s2[]) {
     
 }
 
-/*
-Takes two arguments, const char toSearch[] and const char find[].
-If searching for an empty string, returns toSearch.
-If nothing was found, returns a null pointer.
-*/
 char* Str_search(const char toSearch[], const char find[]) {
     int searchLength = (int)Str_getLength(toSearch);
     int findLength = (int)Str_getLength(find);
