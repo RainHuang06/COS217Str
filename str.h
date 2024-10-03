6e33c1a3b7c285074dc9816c7b73dfe6b0f049cc
Rain Huang <rh2378@princeton.edu>
2024-10-02 22:53:40 -0400

    simply fixing declarations

--- str.h
+++ str.h
@@ -2,8 +2,8 @@
 #define STRING_H
 #include <stddef.h>
 size_t Str_getLength(const char s[]);
-char* Str_copy(char copy[], char original[]);
-char* Str_concat(char copy[], char original[]);
+char* Str_copy(char copy[], const char original[]);
+char* Str_concat(char copy[], const char original[]);
 int Str_compare(const char s1[], const char s2[]);
 char* Str_search(const char toSearch[], const char find[]);
 #endif
